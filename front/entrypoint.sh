d21d9d95006288a52147434b7e359938391a3a51
Raphael Menard <raphael.menard@ynov.com>
2026-06-25 14:46:55 +0200

    fix: use placeholder pattern in entrypoint sed to avoid pipe delimiter conflict

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -2,8 +2,8 @@
 CORE="${CORE_URL:-}"
 AUTH="${AUTH_URL:-}"
 
-sed -i "s|window.CORE_URL || \"\"|window.CORE_URL || \"${CORE}\"|g" /usr/share/nginx/html/index.html
-sed -i "s|window.AUTH_URL || \"\"|window.AUTH_URL || \"${AUTH}\"|g" /usr/share/nginx/html/index.html
+sed -i "s|__CORE_URL__|${CORE}|g" /usr/share/nginx/html/index.html
+sed -i "s|__AUTH_URL__|${AUTH}|g" /usr/share/nginx/html/index.html
 
 PORT="${PORT:-8080}"
 sed -i "s/listen\s*8080;/listen ${PORT};/g" /etc/nginx/conf.d/default.conf
